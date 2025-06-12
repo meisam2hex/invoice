0589bd1d7612d3b602018e63df77745e27210f65
Meisam <meisamkargar@gmail.com>
2025-06-13 01:40:42 +0330

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/molad-factor-ghahveh-saz/', // IMPORTANT: Change if your GitHub repo name is different!
+  base: '/invoice/', // IMPORTANT: Change if your GitHub repo name is different!
 })
